79fbacc582f017af0e6e104e54e490a67a9e3271
Samantha Kong <samangelk@gmail.com>
2023-03-21 00:23:04 -0700

    add error log

--- src/Cart.java
+++ src/Cart.java
@@ -21,11 +21,11 @@ public class Cart {
 //It should be called when the shopper selects option 4 in the menu and should print
 // the total pre-tax cost of all the items in the cart and the cart items themselves.
 // It should also display the after tax cost on a separate line.
-    public void showDetails(Product p){
-        System.out.println("Item Count: " + items.length.toString);
+    public void showDetails(ArrayList items){
+        System.out.println(String.format("Item Count: %d", items.length.toString));
         System.out.println("Items:");
         total = 0;
-        for(Product items : item){
+        for(ArrayList items : item){
             System.out.println(String.format("%s : $%.2f",item.getName(), item.getPrice()));
             total.addPrice(item.getPrice());
         }
